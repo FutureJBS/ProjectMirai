a76ec5e164978edbe14b1caf57a7b42e9c030729
gergomarcellturi <43417257+gergomarcellturi@users.noreply.github.com>
2019-09-30 12:40:34 +0200

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="css/general.css">
+<head>
+	<title>MiraiJobs</title>
+</head>
+<body>
+
+	<h1 class="general">Welcome to MiraiJobs!</h1>
+
+
+	<p class="general">Welcome</p>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<head>
+	<title>Header</title>
+</head>
+<body>
+
+	
+
+	<div class="header">
+		<div class="container"> 
+			<div class="logo">
+			<h1><a href="#">MiraiJobs</a></h1>
+			</div>
+		
+
+		<div>
+			<ul class="nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Registration</a></li>
+				<li><a href="#">Log in</a></li>
+				<li><a href="#">About us</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+		</div>
+
+	</div>
+
+</body>
+</html>
